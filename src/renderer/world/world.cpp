a47ffe4ec9a9b92a10586245c0317ac65141daa5
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2025-01-19 19:52:27 +0300

    Added World

--- src/renderer/world/world.cpp
+++ src/renderer/world/world.cpp
@@ -1,1 +1,14 @@
 #include "world.h"
+
+void World::addObject(const Object& aObject) {
+    WorldObject worldObject = {
+        .object = aObject,
+        .delta = Vec3(0, 0, 0),
+        .rotateMatrix = Mat3()
+    };
+    worldObject.rotateMatrix <<
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0;
+    worldObjects_.push_back(worldObject);
+}
--- src/renderer/world/world.h
+++ src/renderer/world/world.h
@@ -1,1 +1,17 @@
-#include <SFML/Graphics.hpp>
+#include <renderer/object/object.h>
+#include <renderer/utils/linalg.h>
+
+class World {
+public:
+    World() = default;
+    void addObject(const Object& aObject);
+
+private:
+    struct WorldObject {
+        Object object;
+        Vec3 delta;
+        Mat3 rotateMatrix;
+    };
+
+    std::vector<WorldObject> worldObjects_;
+};
