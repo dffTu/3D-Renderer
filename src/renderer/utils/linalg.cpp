e2dfbc96f8b1d66a339ddebf8d92c0f46b24a907
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2025-01-25 15:15:48 +0300

    Replace transform matrixes to 4-dimension

--- src/renderer/utils/linalg.cpp
+++ src/renderer/utils/linalg.cpp
@@ -1,37 +1,51 @@
 #include "linalg.h"
 
-Mat3 getDefaultMat3() {
-    Mat3 result;
+Mat4 getDefaultMat4() {
+    Mat4 result;
     result <<
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0;
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0;
     return result;
 }
 
-Mat3 getXRotationMatrix(double aRadians) {
-    Mat3 result;
+Mat4 getMoveMatrix(const Vec3& aVec3) {
+    Mat4 result;
     result <<
-        1.0, 0.0, 0.0,
-        0.0, cos(aRadians), -sin(aRadians),
-        0.0, sin(aRadians), cos(aRadians);
+        1.0, 0.0, 0.0, aVec3[0],
+        0.0, 1.0, 0.0, aVec3[1],
+        0.0, 0.0, 1.0, aVec3[2],
+        0.0, 0.0, 0.0, 1.0;
     return result;
 }
 
-Mat3 getYRotationMatrix(double aRadians) {
-    Mat3 result;
+Mat4 getXRotationMatrix(double aRadians) {
+    Mat4 result;
     result <<
-        cos(aRadians), 0.0, sin(aRadians),
-        0.0, 1.0, 0.0,
-        -sin(aRadians), 0.0, cos(aRadians);
+        1.0, 0.0, 0.0, 0.0,
+        0.0, cos(aRadians), -sin(aRadians), 0.0,
+        0.0, sin(aRadians), cos(aRadians), 0.0,
+        0.0, 0.0, 0.0, 1.0;
     return result;
 }
 
-Mat3 getZRotationMatrix(double aRadians) {
-    Mat3 result;
+Mat4 getYRotationMatrix(double aRadians) {
+    Mat4 result;
     result <<
-        cos(aRadians), -sin(aRadians), 0.0,
-        sin(aRadians), cos(aRadians), 0.0,
-        0.0, 0.0, 1.0;
+        cos(aRadians), 0.0, sin(aRadians), 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        -sin(aRadians), 0.0, cos(aRadians), 0.0,
+        0.0, 0.0, 0.0, 1.0;
+    return result;
+}
+
+Mat4 getZRotationMatrix(double aRadians) {
+    Mat4 result;
+    result <<
+        cos(aRadians), -sin(aRadians), 0.0, 0.0,
+        sin(aRadians), cos(aRadians), 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0;
     return result;
 }
--- src/renderer/utils/linalg.h
+++ src/renderer/utils/linalg.h
@@ -7,7 +7,8 @@ using Vec4 = Eigen::Vector4f;
 using Mat3 = Eigen::Matrix3f;
 using Mat4 = Eigen::Matrix4f;
 
-Mat3 getDefaultMat3();
-Mat3 getXRotationMatrix(double aRadians);
-Mat3 getYRotationMatrix(double aRadians);
-Mat3 getZRotationMatrix(double aRadians);
+Mat4 getDefaultMat4();
+Mat4 getMoveMatrix(const Vec3& aVec3);
+Mat4 getXRotationMatrix(double aRadians);
+Mat4 getYRotationMatrix(double aRadians);
+Mat4 getZRotationMatrix(double aRadians);
