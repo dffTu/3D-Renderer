169fa94b4517b4854046533c30d94986c79d42d7
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2025-01-25 13:49:53 +0300

    Added some util funcs

--- src/renderer/utils/linalg.cpp
+++ src/renderer/utils/linalg.cpp
@@ -1,1 +1,37 @@
 #include "linalg.h"
+
+Mat3 getDefaultMat3() {
+    Mat3 result;
+    result <<
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0;
+    return result;
+}
+
+Mat3 getXRotationMatrix(double aRadians) {
+    Mat3 result;
+    result <<
+        1.0, 0.0, 0.0,
+        0.0, cos(aRadians), -sin(aRadians),
+        0.0, sin(aRadians), cos(aRadians);
+    return result;
+}
+
+Mat3 getYRotationMatrix(double aRadians) {
+    Mat3 result;
+    result <<
+        cos(aRadians), 0.0, sin(aRadians),
+        0.0, 1.0, 0.0,
+        -sin(aRadians), 0.0, cos(aRadians);
+    return result;
+}
+
+Mat3 getZRotationMatrix(double aRadians) {
+    Mat3 result;
+    result <<
+        cos(aRadians), -sin(aRadians), 0.0,
+        sin(aRadians), cos(aRadians), 0.0,
+        0.0, 0.0, 1.0;
+    return result;
+}
