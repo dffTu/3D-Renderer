7e6c45408b1d961c3c2b10b383e8ad600b719ad1
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2025-01-25 13:31:00 +0300

    Add getters to Camera class

--- src/renderer/camera/camera.cpp
+++ src/renderer/camera/camera.cpp
@@ -9,3 +9,11 @@ Camera::Camera(const Vec3& aPosition, const Vec3& aDirection) {
     position_ = aPosition;
     direction_ = aDirection;
 }
+
+Vec3 Camera::getPosition() const {
+    return position_;
+}
+
+Vec3 Camera::getDirection() const {
+    return direction_;
+}
--- src/renderer/camera/camera.h
+++ src/renderer/camera/camera.h
@@ -5,6 +5,9 @@ public:
     Camera();
     Camera(const Vec3& aPosition, const Vec3& aDirection);
 
+    Vec3 getPosition() const;
+    Vec3 getDirection() const;
+
 private:
     Vec3 position_;
     Vec3 direction_;
